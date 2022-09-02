9452fd325ea74ba91015d9e4231d8514d5b09f14
DobruyPK <65114650+DobruyPK@users.noreply.github.com>
2022-09-02 22:39:01 +0500

    asdasdasd

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=['localhost', '127.0.0.1', 'testserver'])
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=['localhost', '62.84.114.109', 'testserver'])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
